8c1293b5a098d64f0d069c286d071d270e4cd069
MineBill <terrestrialstudio@gmail.com>
2020-11-04 16:30:20 +0200

    add: Created a new font script to quickly install compressed font files

---
+++ scripts/scripts/font.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+INSTALL_LOCATION="~/.local/share/fonts/"
+
+if [[ $# -ge 2 ]]; then
+    if [[ -d "$2" ]]; then
+        INSTALL_LOCATION=$2
+    else
+        echo "$2 is not a valid location"
+        echo "Exiting.."
+        exit -1
+    fi
+else
+    if [[ ! -d "$INSTALL_LOCATION" ]]; then
+        echo "$INSTALL_LOCATION does not exist"
+        echo "Should i create it(Y/n)?"
+        read $ans
+        and=${ans:y}
+        case $ans in
+            y)
+                mkdir -p $INSTALL_LOCATION
+                ;;
+            *)
+                echo "Exiting.."
+                exit 0
+                ;;
+        esac
+    fi
+fi
+
+if [[ ! -f "$1" ]]; then
+    echo "$1 does not exist."
+    echo "Exiting"
+    exit -1
+fi
+
+ft=$(echo $1 | grep '\..*' -o)
+
+case $ft in 
+    .zip)
+        echo "Installing zip file to " "\"$INSTALL_LOCATION\""
+        unzip $1 -d $INSTALL_LOCATION 1>/dev/null
+        ;;
+    .tar.gz)
+        echo "Installing zip file to " "\"$INSTALL_LOCATION\""
+        tar -xvzf $1 -C $INSTALL_LOCATION 1>/dev/null
+        ;;
+    *)
+        echo "Filetype is not supported"
+        ;;
+esac
