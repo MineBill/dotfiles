955e6d96144c95524f2b8bab8981811463e94112
MineBill <minebill@protonmail.com>
2021-06-16 23:57:23 +0300

    Added polybar and mozilla stuff

---
+++ mozilla/user.js
@@ -0,0 +1,23 @@
+// ** Theme Related Options ****************************************************
+// userchrome.css usercontent.css activate
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+
+// Fill SVG Color
+user_pref("svg.context-properties.content.enabled", true);
+
+// CSS Blur Filter - 88 Above
+user_pref("layout.css.backdrop-filter.enabled", true);
+
+// Restore Compact Mode - 89 Above
+user_pref("browser.compactmode.show", true);
+
+// about:home Search Bar
+// user_pref("browser.newtabpage.activity-stream.improvesearch.handoffToAwesomebar", false);
+
+// Enable closetab to always be displayed
+user_pref("browser.tabs.tabMinWidth", 0);
+user_pref("browser.tabs.tabClipWidth", 0);
+
+// ** Useful Options ***********************************************************
+// Integrated calculator at urlbar
+user_pref("browser.urlbar.suggest.calculator", true);
