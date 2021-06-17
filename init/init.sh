ac8e4f0f30b5dcb5507d0eead7487308bc749de1
MineBill <minebill@protonmail.com>
2021-06-17 18:17:17 +0300

    Changes

--- init/init.sh
+++ init/init.sh
@@ -5,8 +5,7 @@ KEY_REPEAT_RATE_HZ=30
 KEY_REPEAT_DELAY_MS=250
 PRIMARY_SCREEN=HDMI-0
 
-#imwheel
-xrandr --output DVI-D-0 --auto --left-of HDMI-0
+# xrandr --output DVI-D-0 --auto --left-of HDMI-0
 xrandr --output $PRIMARY_SCREEN --primary --mode 1920x1080 --rate 143.98
 xinput --set-prop 9 'libinput Accel Profile Enabled' 0, 1
 xinput --set-prop 9 'libinput Accel Speed' 0.3
